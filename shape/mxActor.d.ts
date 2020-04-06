cef144d626728aa6252c7db62bba24249d2b44d8
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-06 19:06:28 +0200

    feat: add some missing shapes
    Add
      - mxActor
      - mxArrow
      - mxArrowConnector
      - mxCloud
      - mxCylinder
      - mxDoubleEllipse

---
+++ shape/mxActor.d.ts
@@ -0,0 +1,42 @@
+///<reference path="mxShape.d.ts"/>
+///<reference path="../util/mxAbstractCanvas2D.d.ts"/>
+///<reference path="../util/mxRectangle.d.ts"/>
+/**
+ * Extends {@link mxShape} to implement an actor shape. If a custom shape with one
+ * filled area is needed, then this shape's {@link redrawPath} method should be overridden.
+ *
+ * This shape is registered under {@link mxConstants.SHAPE_ACTOR} in {@link mxCellRenderer}.
+ *
+ * @example
+ * ```javascript
+ * function SampleShape() { }
+ *
+ * SampleShape.prototype = new mxActor();
+ * SampleShape.prototype.constructor = vsAseShape;
+ *
+ * mxCellRenderer.registerShape('sample', SampleShape);
+ * SampleShape.prototype.redrawPath = function(path, x, y, w, h)
+ * {
+ *   path.moveTo(0, 0);
+ *   path.lineTo(w, h);
+ *   // ...
+ *   path.close();
+ * }
+ * ```
+ */
+declare class mxActor extends mxShape {
+  /**
+   * Constructs a new actor shape.
+   *
+   * @param bounds         {@link mxRectangle} that defines the bounds. This is stored in {@link mxShape.bounds}.
+   * @param fill           String that defines the fill color. This is stored in {@link mxShape.fill}.
+   * @param stroke         String that defines the stroke color. This is stored in {@link mxShape.stroke}.
+   * @param strokewidth    Optional integer that defines the stroke width. Default is 1. This is stored in {@link mxShape.strokewidth}.
+   */
+  constructor(bounds: mxRectangle, fill: string, stroke: string, strokewidth?: number);
+
+  /**
+   * Draws the path for this shape.
+   */
+  redrawPath(c: mxAbstractCanvas2D, x: number, y: number, w: number, h: number): void;
+}
