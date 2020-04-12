d9c578812298e076f72064d05b507e7c97b04b68
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-13 01:54:15 +0200

    WIP mxEdgeLabelLayout

--- layout/index.d.ts
+++ layout/index.d.ts
@@ -2,6 +2,7 @@
 /// <reference path="./mxCircleLayout.d.ts" />
 /// <reference path="./mxCompactTreeLayout.d.ts" />
 /// <reference path="./mxCompositeLayout.d.ts" />
+/// <reference path="./mxEdgeLabelLayout.d.ts" />
 /// <reference path="./mxFastOrganicLayout.d.ts" />
 /// <reference path="./mxGraphLayout.d.ts" />
 /// <reference path="./mxParallelEdgeLayout.d.ts" />
