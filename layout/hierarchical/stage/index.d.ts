eed6c6bdbe600d79dca8863bd357659902f38823
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-13 14:44:24 +0200

    add mxMinimumCycleRemover

--- layout/hierarchical/stage/index.d.ts
+++ layout/hierarchical/stage/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="./mxCoordinateAssignment.d.ts" />
 /// <reference path="./mxHierarchicalLayoutStage.d.ts" />
+/// <reference path="./mxMinimumCycleRemover.d.ts" />
 /// <reference path="./mxSwimlaneOrdering.d.ts" />
