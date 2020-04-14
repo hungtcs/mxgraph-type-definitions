0c1e49d08e199bf8dc08c783a4cf65e5881ad434
鸿则 <hungtcs@163.com>
2020-04-14 11:43:50 +0800

    feat: update view types
    - modified:   view/index.d.ts
    - modified:   view/mxMultiplicity.d.ts
    - new file:   view/mxOutline.d.ts
    - modified:   view/mxPerimeter.d.ts
    - modified:   view/mxPrintPreview.d.ts
    - new file:   view/mxStyleRegistry.d.ts
    - modified:   view/mxStylesheet.d.ts
    - modified:   view/mxSwimlaneManager.d.ts
    - new file:   view/mxTemporaryCellStates.d.ts

--- view/mxMultiplicity.d.ts
+++ view/mxMultiplicity.d.ts
@@ -1,5 +1,49 @@
-
+/**
+ * @class mxMultiplicity
+ *
+ * Defines invalid connections along with the error messages that they produce.
+ * To add or remove rules on a graph, you must add/remove instances of this
+ * class to {@link mxGraph.multiplicities}.
+ *
+ * ### Example
+ *
+ * @example
+ * ```javascript
+ * graph.multiplicities.push(new mxMultiplicity(
+ *   true, 'rectangle', null, null, 0, 2, ['circle'],
+ *   'Only 2 targets allowed',
+ *   'Only circle targets allowed'));
+ * ```
+ *
+ * Defines a rule where each rectangle must be connected to no more than 2
+ * circles and no other types of targets are allowed.
+ */
 declare class mxMultiplicity {
+
+  /**
+   * @constructor
+   *
+   * Instantiate class mxMultiplicity in order to describe allowed
+   * connections in a graph. Not all constraints can be enforced while
+   * editing, some must be checked at validation time. The {@link countError} and
+   * {@link typeError} are treated as resource keys in {@link mxResources}.
+   *
+   * @param source Boolean indicating if this rule applies to the source or target
+   * terminal.
+   * @param type Type of the source or target terminal that this rule applies to.
+   * See {@link type} for more information.
+   * @param attr Optional attribute name to match the source or target terminal.
+   * @param value Optional attribute value to match the source or target terminal.
+   * @param min Minimum number of edges for this rule. Default is 1.
+   * @param max Maximum number of edges for this rule. n means infinite. Default
+   * is n.
+   * @param validNeighbors Array of types of the opposite terminal for which this
+   * rule applies.
+   * @param countError Error to be displayed for invalid number of edges.
+   * @param typeError Error to be displayed for invalid opposite terminals.
+   * @param validNeighborsAllowed Optional boolean indicating if the array of
+   * opposite types should be valid or invalid.
+   */
   constructor(
     source: boolean,
     type: string,
@@ -7,133 +51,107 @@ declare class mxMultiplicity {
     value: string,
     min: number,
     max: number,
-    validNeighbors: string[],
+    validNeighbors: Array<string>,
     countError: string,
     typeError: string,
-    validNeighborsAllowed: boolean
+    validNeighborsAllowed: boolean,
   );
 
   /**
-   * Variable: type
-   *
    * Defines the type of the source or target terminal. The type is a string
-   * passed to <mxUtils.isNode> together with the source or target vertex
+   * passed to {@link mxUtils.isNode} together with the source or target vertex
    * value as the first argument.
    */
   type: string;
 
   /**
-   * Variable: attr
-   *
    * Optional string that specifies the attributename to be passed to
-   * <mxUtils.isNode> to check if the rule applies to a cell.
+   * {@link mxUtils.isNode} to check if the rule applies to a cell.
    */
   attr: string;
 
   /**
-   * Variable: value
-   *
    * Optional string that specifies the value of the attribute to be passed
-   * to <mxUtils.isNode> to check if the rule applies to a cell.
+   * to {@link mxUtils.isNode} to check if the rule applies to a cell.
    */
   value: string;
 
   /**
-   * Variable: source
-   *
    * Boolean that specifies if the rule is applied to the source or target
    * terminal of an edge.
    */
-  source: string;
+  source: boolean;
 
   /**
-   * Variable: min
-   *
    * Defines the minimum number of connections for which this rule applies.
-   * Default is 0.
+   *
+   * @default 0
    */
   min: number;
 
   /**
-   * Variable: max
-   *
    * Defines the maximum number of connections for which this rule applies.
-   * A value of 'n' means unlimited times. Default is 'n'.
+   * A value of 'n' means unlimited times.
+   * @default 'n'
    */
   max: number | 'n';
 
   /**
-   * Variable: validNeighbors
-   *
    * Holds an array of strings that specify the type of neighbor for which
-   * this rule applies. The strings are used in <mxCell.is> on the opposite
+   * this rule applies. The strings are used in {@link mxCell.is} on the opposite
    * terminal to check if the rule applies to the connection.
    */
-  validNeighbors: string[];
+  validNeighbors: Array<string>;
 
   /**
-   * Variable: validNeighborsAllowed
-   *
    * Boolean indicating if the list of validNeighbors are those that are allowed
    * for this rule or those that are not allowed for this rule.
    */
   validNeighborsAllowed: boolean;
 
   /**
-   * Variable: countError
-   *
    * Holds the localized error message to be displayed if the number of
-   * connections for which the rule applies is smaller than <min> or greater
-   * than <max>.
+   * connections for which the rule applies is smaller than {@link min} or greater
+   * than {@link max}.
    */
   countError: string;
 
   /**
-   * Variable: typeError
-   *
    * Holds the localized error message to be displayed if the type of the
    * neighbor for a connection does not match the rule.
    */
   typeError: string;
 
   /**
-   * Function: check
-   *
    * Checks the multiplicity for the given arguments and returns the error
    * for the given connection or null if the multiplicity does not apply.
    *
-   * Parameters:
-   *
-   * graph - Reference to the enclosing <mxGraph> instance.
-   * edge - <mxCell> that represents the edge to validate.
-   * source - <mxCell> that represents the source terminal.
-   * target - <mxCell> that represents the target terminal.
-   * sourceOut - Number of outgoing edges from the source terminal.
-   * targetIn - Number of incoming edges for the target terminal.
+   * @param graph Reference to the enclosing {@link mxGraph} instance.
+   * @param edge {@link mxCell} that represents the edge to validate.
+   * @param source {@link mxCell} that represents the source terminal.
+   * @param target {@link mxCell} that represents the target terminal.
+   * @param sourceOut Number of outgoing edges from the source terminal.
+   * @param targetIn Number of incoming edges for the target terminal.
    */
-  check(graph: mxGraph, edge: mxCell, source: mxCell, target: mxCell, sourceOut: number, targetIn: number): string[];
+  check(graph: mxGraph, edge: mxCell, source: mxCell, target: mxCell,
+    sourceOut: number, targetIn: number): string;
 
   /**
-   * Function: checkNeighbors
-   *
-   * Checks if there are any valid neighbours in <validNeighbors>. This is only
-   * called if <validNeighbors> is a non-empty array.
+   * Checks if there are any valid neighbours in {@link validNeighbors}. This is only
+   * called if {@link validNeighbors} is a non-empty array.
    */
   checkNeighbors(graph: mxGraph, edge: mxCell, source: mxCell, target: mxCell): boolean;
 
   /**
-   * Function: checkTerminal
-   *
    * Checks the given terminal cell and returns true if this rule applies. The
    * given cell is the source or target of the given edge, depending on
-   * <source>. This implementation uses <checkType> on the terminal's value.
+   * {@link source}. This implementation uses {@link checkType} on the terminal's value.
    */
   checkTerminal(graph: mxGraph, terminal: mxCell, edge: mxCell): boolean;
 
   /**
-   * Function: checkType
-   *
    * Checks the type of the given value.
    */
-  checkType(graph: mxGraph, value: mxCell, type: string, attr: string, attrValue: string): boolean;
+  checkType(graph: mxCell, value: string, type: string, attr: string, attrValue: any): boolean;
+
 }
--- view/mxSwimlaneManager.d.ts
+++ view/mxSwimlaneManager.d.ts
@@ -1,86 +1,105 @@
-
 /**
- * Manager for swimlanes and nested swimlanes that sets the size of
- * newly added swimlanes to that of their siblings,
- * and propagates changes to the size of a swimlane to its siblings,
- * if <siblings> is true, and its ancestors, if <bubbling> is true.
+ * @class mxSwimlaneManager
+ * @extends mxEventSource
+ *
+ * Manager for swimlanes and nested swimlanes that sets the size of newly added
+ * swimlanes to that of their siblings, and propagates changes to the size of a
+ * swimlane to its siblings, if {@link siblings} is true, and its ancestors, if
+ * {@link bubbling} is true.
  */
-declare class mxSwimlaneManager {
+declare class mxSwimlaneManager extends mxEventSource {
 
   /**
-   * Reference to the enclosing mxGraph.
+   * Constructs a new swimlane manager for the given graph.
+   *
+   * @param {mxGraph} graph             Reference to the enclosing graph.
+   * @param {boolean} [horizontal]
+   * @param {boolean} [addEnabled]
+   * @param {boolean} [resizeEnabled]
+   */
+  constructor(graph: mxGraph, horizontal?: boolean, addEnabled?: boolean, resizeEnabled?: boolean);
+
+  /**
+   * Reference to the enclosing {@link mxGraph}.
    */
   graph: mxGraph;
 
   /**
    * Specifies if event handling is enabled.
+   * @default true
    */
-  enabled: boolean
+  enabled: boolean;
 
   /**
    * Specifies the orientation of the swimlanes.
+   * @default true
    */
   horizontal: boolean;
 
   /**
-   * Specifies if newly added cells should be resized to match the size of their existing siblings.
+   * Specifies if newly added cells should be resized to match the size of their
+   * existing siblings.
+   * @default true
    */
   addEnabled: boolean;
 
   /**
    * Specifies if resizing of swimlanes should be handled.
+   * @default true
    */
   resizeEnabled: boolean;
 
   /**
    * Holds the function that handles the move event.
    */
-  moveHandler: any;
-
-  constructor(
-    graph: mxGraph,
-    horizontal?: boolean,
-    addEnabled?: boolean,
-    resizeEnabled?: boolean
-  );
+  addHandler: Function;
 
   /**
-   * Returns true if events are handled.
+   * Holds the function that handles the move event.
+   */
+  resizeHandler: Function;
+
+  /**
+   * Returns true if events are handled. This implementation
+   * returns {@link enabled}.
    */
   isEnabled(): boolean;
 
   /**
-   * Enables or disables event handling.
+   * Enables or disables event handling. This implementation
+   * updates {@link enabled}.
+   *
+   * @param enabled Boolean that specifies the new enabled state.
    */
   setEnabled(value: boolean): void;
 
   /**
-   * Returns horizontal.
+   * Returns {@link horizontal}.
    */
   isHorizontal(): boolean;
 
   /**
-   * Sets horizontal.
+   * Sets {@link horizontal}.
    */
   setHorizontal(value: boolean): void;
 
   /**
-   * Returns addEnabled.
+   * Returns {@link addEnabled}.
    */
   isAddEnabled(): boolean;
 
   /**
-   * Sets addEnabled.
+   * Sets {@link addEnabled}.
    */
   setAddEnabled(value: boolean): void;
 
   /**
-   * Returns resizeEnabled.
+   * Returns {@link resizeEnabled}.
    */
   isResizeEnabled(): boolean;
 
   /**
-   * Sets resizeEnabled.
+   * Sets {@link resizeEnabled}.
    */
   setResizeEnabled(value: boolean): void;
 
@@ -97,35 +116,49 @@ declare class mxSwimlaneManager {
   /**
    * Returns true if the given swimlane should be ignored.
    */
-  isSwimlaneIgnored(swimlane: any): boolean;
+  isSwimlaneIgnored(swimlane: mxCell): boolean;
 
   /**
-   * Returns true if the given cell is horizontal.
+   * Returns true if the given cell is horizontal. If the given cell is not a
+   * swimlane, then the global orientation is returned.
    */
   isCellHorizontal(cell: mxCell): boolean;
 
   /**
    * Called if any cells have been added.
+   *
+   * @param cell Array of {@link mxCells} that have been added.
    */
   cellsAdded(cells: Array<mxCell>): void;
 
   /**
-   * Updates the size of the given swimlane to match that of any existing siblings swimlanes.
+   * Updates the size of the given swimlane to match that of any existing
+   * siblings swimlanes.
+   *
+   * @param swimlane {@link mxCell} that represents the new swimlane.
    */
-  swimlaneAdded(swimlane: any): void;
+  swimlaneAdded(swimlane: mxCell): void;
 
   /**
-   * Called if any cells have been resizes.
+   * Called if any cells have been resizes. Calls {@link swimlaneResized} for all
+   * swimlanes where {@link isSwimlaneIgnored} returns false.
+   *
+   * @param cells Array of {@link mxCells} whose size was changed.
    */
   cellsResized(cells: Array<mxCell>): void;
 
   /**
-   * Called from cellsResized for all swimlanes that are not ignored to update the size of the siblings and the size of the parent swimlanes, recursively, if <bubbling> is true.
+   * Called from {@link cellsResized} for all swimlanes that are not ignored to update
+   * the size of the siblings and the size of the parent swimlanes, recursively,
+   * if {@link bubbling} is true.
+   *
+   * @param swimlane {@link mxCell} whose size has changed.
    */
-  resizeSwimlane(swimlane: any, w: number, h: number, parentHorizontal: boolean): void;
+  resizeSwimlane(swimlane: mxCell, w: number, h: number, parentHorizontal: boolean): void;
 
   /**
-   * Removes all handlers from the graph and deletes the reference to it.
+   * Removes all handlers from the {@link graph} and deletes the reference to it.
    */
   destroy(): void;
+
 }
